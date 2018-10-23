baf8fb7c1a3dabd19272e2d720f29ab1899c709a
Krishna Gummadi <swethatammareddy@Swethas-MacBook-Pro.local>
2018-10-23 22:19:58 +1100

    dockerizing samples

---
+++ build/precache-manifest.3ba23fd53eeaa07efd66a125d593920d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bad211b8a5cfadd9f868",
+    "url": "/static/js/main.bad211b8.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "bad211b8a5cfadd9f868",
+    "url": "/static/css/main.eabdcb20.chunk.css"
+  },
+  {
+    "revision": "7901f13c8d26c9e7f890f0a1a31727d7",
+    "url": "/index.html"
+  }
+];
